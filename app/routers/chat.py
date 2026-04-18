6db68053aad8563dae682f26f558c51b8547a6fb
RupeshLineseDev <rupesh.rajhans92@gmail.com>
2026-04-18 07:19:37 +0530

    add structured output code

--- app/routers/chat.py
+++ app/routers/chat.py
@@ -1,16 +1,21 @@
 from fastapi import APIRouter, HTTPException, Depends
 from app.schema.toolCalling import HeaderDetail, RequestBody
 from app.helpers.middleware import get_header_details
+from app.ai.aiController import AIController
+
+llmModel = AIController()
 
 router = APIRouter(prefix="/toolcalling", tags=["toolcalling"])
 
 @router.post("/call")
 async def tool_calling(request :RequestBody, getHeaderDetail: HeaderDetail = Depends(get_header_details)):
     try:
-
+        structuredReturn = llmModel.parseUserData(request.message)
+        toolDataReturn = llmModel.executeTool(structuredReturn)
+        llmCallReturn = llmModel.callLLM(toolDataReturn)
         return {
             "status": "success",
-            "message": f"Tool calling endpoint is working fine {getHeaderDetail['userId']}! You sent the message: {request.message}"
+            "message": f"T{llmCallReturn}"
         }
     except Exception as e:
         raise HTTPException( status_code=500, detail="The AI service failed to process the request. Please try again later." )
